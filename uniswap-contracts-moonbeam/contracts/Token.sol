5a0399339c1903a06fd4cd62d74c195c8201880f
ahmetson <ahmetson@zoho.com>
2021-08-19 18:23:50 +0700

    Update Token for samples

--- uniswap-contracts-moonbeam/contracts/Token.sol
+++ uniswap-contracts-moonbeam/contracts/Token.sol
@@ -10,7 +10,7 @@ contract Token is ERC20Detailed, ERC20Mintable {
     uint256 public MINTED_AMOUNT = 5 ether;
     
     constructor(string memory _name, string memory _symbol) ERC20Detailed(_name, _symbol, 18) public {
-        _mint(msg.sender, 1000000000000000000000000);
+        _mint(msg.sender, 10000 * 1**18);
     }
     
     function mintTokens() public {
