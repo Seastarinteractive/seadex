5cb0d73399fd1c888ffddb923ec2fdd333b9708f
ahmetson <ahmetson@zoho.com>
2021-08-19 18:27:09 +0700

    Add script to deploy sample ERC-20 tokens: Medet, Blocklords

---
+++ uniswap-contracts-moonbeam/scripts/deploy-tokens.js
@@ -0,0 +1,27 @@
+const { ethers } = require('hardhat');
+
+// Deploy function
+async function deploy() {
+   [account] = await ethers.getSigners();
+   deployerAddress = account.address;
+   console.log(`Deploying contracts using ${deployerAddress}`);
+
+   // Deploy WETH
+   const token = await ethers.getContractFactory('Token');
+   const mdt = await token.deploy("MEDET", "MDT");
+   await mdt.deployed();
+
+   console.log(`MEDET token deployed to : ${mdt.address}`);
+
+   const bl = await token.deploy("BLOCKLORDS ERC20", "BL20");
+   await bl.deployed();
+
+   console.log(`Blocklords ERC-20 token deployed to : ${bl.address}`);
+}
+
+deploy()
+   .then(() => process.exit(0))
+   .catch((error) => {
+      console.error(error);
+      process.exit(1);
+   });
