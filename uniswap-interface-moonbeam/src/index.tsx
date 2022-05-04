a394fbee8b07ba14ac870fe1306db49a64a96dc0
LevanOzashvili <ozashvili1@gmail.com>
2022-05-04 17:34:29 +0400

    setup GA.

--- uniswap-interface-moonbeam/src/index.tsx
+++ uniswap-interface-moonbeam/src/index.tsx
@@ -20,10 +20,11 @@ import getLibrary from './utils/getLibrary'
 const Web3ProviderNetwork = createWeb3ReactRoot(NetworkContextName)
 
 if ('ethereum' in window) {
-  ;(window.ethereum as any).autoRefreshOnNetworkChange = false
+  ; (window.ethereum as any).autoRefreshOnNetworkChange = false
 }
-
-const GOOGLE_ANALYTICS_ID: string | undefined = process.env.REACT_APP_GOOGLE_ANALYTICS_ID
+const GOOGLE_ANALYTICS_ID = 'UA-227851748-2'
+// const GOOGLE_ANALYTICS_ID: string | undefined = process.env.REACT_APP_GOOGLE_ANALYTICS_ID
+console.log('from config: ', process.env.REACT_APP_GOOGLE_ANALYTICS_ID)
 if (typeof GOOGLE_ANALYTICS_ID === 'string') {
   ReactGA.initialize(GOOGLE_ANALYTICS_ID)
   ReactGA.set({
